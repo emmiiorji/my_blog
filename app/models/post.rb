d5ee95b2b765d84a4401c6f55ca9ed0f912b3e69
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-16 19:05:17 +0100

    Add comment to update posts count in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to: user, class_name: 'User'
   has_many: comments
   has_many: likes
+
+  private
+
+  def update_posts_count
+    author.update(posts_counter: author.posts.count)
+  end
 end
