f24a5585b7ed3bfec043dc1ca4ec4eeb4d7ab692
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-16 19:05:59 +0100

    Add method to get recent comments in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,8 @@ class Post < ApplicationRecord
   def update_posts_count
     author.update(posts_counter: author.posts.count)
   end
+
+  def recent_comments(n = 5)
+    comments.includes(:post).order(created_at: :desc).limit(n)
+  end
 end
