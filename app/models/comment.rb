fbc076b7e8757b710fa9463746a231325ac8040f
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-16 19:20:47 +0100

    Fix rubocop errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
-  belongs_to: author, class_name: 'User'
-  belongs_to: post
-  after_save: update_comments_count
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+  after_save :update_comments_count
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_many: posts, foreign_key: 'author_id'
-  has_many: likes, foreign_key: 'author_id'
-  has_many: comments, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
 
   def recent_posts(n = 3)
     posts.includes(:author).order(created_at: :desc).limit(n)
   end
-ends
+end
