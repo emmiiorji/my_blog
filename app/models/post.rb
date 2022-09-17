5673053f5de1751b493ebd6e479e32009b43e3b7
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-17 15:55:02 +0100

    Add validation for comments_counter in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,8 @@ class Post < ApplicationRecord
   has_many :likes
   
   validates :title, presence: true, length: { maximum: 250 }
- 
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
   after_save :update_posts_count
 
   private
