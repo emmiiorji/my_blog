ad21ab6d6656888b8f18ccbf38df089e9df1a730
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-18 05:56:28 +0100

    Add test for Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,1 +1,18 @@
 require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  subject do
+    test_user = User.create(name: 'Emmanuel', bio: 'Microverse student', posts_counter: 0)
+    test_post = Post.create(author: test_user, title: 'My first post', text: 'Hello world', comments_counter: 0,
+                            likes_counter: 0)
+    Comment.new(author: test_user, post: test_post, text: 'It is a great post. Thanks')
+  end
+  before { subject.save }
+
+  it 'updates post comment counter' do
+    expect(subject.post.comments_counter).to eq 1
+  end
+  it 'has commentor as Emmanuel' do
+    expect(subject.post.author.name).to eq 'Emmanuel'
+  end
+end
