388bdd2dce0252c522b05e45721e8c0ca00a9e9a
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-18 06:08:40 +0100

    Fix rubocop error

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Like, type: :model do
   subject do
     test_user = User.create(name: 'Emmanuel', bio: 'Microverse student', posts_counter: 0)
-    test_post = Post.create(author_id: test_user.id, title: 'My first post', text: 'I am posting now', comments_counter: 0,
+    test_post = Post.create(author_id: test_user.id, title: 'My first post', text: 'Hello World', comments_counter: 0,
                             likes_counter: 0)
     Like.create(author: test_user, post_id: test_post.id)
   end
