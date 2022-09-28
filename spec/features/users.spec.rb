80899d7dba6b60063603444ee630a114d9da9ace
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-28 22:14:33 +0100

    Add test for user show placeholder text rendered

--- spec/features/users.spec.rb
+++ spec/features/users.spec.rb
@@ -5,4 +5,9 @@ RSpec.describe User do
     get '/users'
     expect(page).to have_text('This is a list of users')
   end
+
+  it 'response body includes correct placeholder text for user show' do
+    get '/users/:id'
+    expect(page).to have_text('This is a single user')
+  end
 end
