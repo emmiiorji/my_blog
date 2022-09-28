b06d8a4bef77b67b6544e372f9e54383f343243d
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-28 22:10:04 +0100

    Add test for posts index route's template rendered

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Post do
     get '/users/:id/posts'
     expect(response).to have_http_status(:ok)
   end
+
+  it 'renders correct template for posts index' do
+    get '/users/:id/posts'
+    expect(response).to render_template('index')
+  end
 end
