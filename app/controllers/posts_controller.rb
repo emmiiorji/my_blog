e9b80970a9a6b43aeb622121003c890d7c85a07f
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-29 20:26:51 +0100

    Complete posts show action method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,10 @@ class PostsController < ApplicationController
     @user = User.find_by(id: params[:user_id])
     @posts = Post.where(author: @user)
   end
+
+  def show
+    @user = User.find_by(id: params[:user_id])
+    @post = Post.find_by(id: params[:id])
+    @comments = Comment.where(post: @post)
+  end
 end
