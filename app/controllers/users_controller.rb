f3366c38b1033e4d81a03038c031e509584b26fc
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-29 20:25:27 +0100

    Modify users show action method for when user_id does not exist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @recent_posts = @user.recent_posts
+    @recent_posts = @user.recent_posts if @user
   end
 end
