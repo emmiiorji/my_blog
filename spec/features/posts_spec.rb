a9700eff6d7d3bc49622430b2d32da2e8b351a3e
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-29 04:17:30 +0100

    Rename features/users.spec.rb to features/users_spec.rb

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Post do
   it 'response body includes correct placeholder text for posts index' do
-    get '/users/:id/posts'
+    visit '/users/:id/posts'
     expect(page).to have_text('This is a list of posts')
   end
 
   it 'response body includes correct placeholder text for post show' do
-    get '/users/:id/posts/:id'
+    visit '/users/:id/posts/:id'
     expect(page).to have_text('This is a single post')
   end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User do
   it 'response body includes correct placeholder text for users index' do
-    get '/users'
+    visit '/users'
     expect(page).to have_text('This is a list of users')
   end
 
   it 'response body includes correct placeholder text for user show' do
-    get '/users/:id'
+    visit '/users/:id'
     expect(page).to have_text('This is a single user')
   end
 end
