4d265b2734b9834ae2108764b993f7658309be97
emmiiorji <emmanuelorji2013@gmail.com>
2022-10-02 09:47:08 +0100

    Modify routes to include create likes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index new create show] do
       resources :comments, only: %i[new create]
+      resources :likes, only: [:create]
     end
   end
 end
