47dcb1e1d02b5227a233ff569451d98f9b6e5466
shakerAbuDrais <thecoffeiner@gmail.com>
2023-02-03 23:15:43 +0300

    Test if user show view shows the last three posts

--- spec/features/users/show.html.erb_spec.rb
+++ spec/features/users/show.html.erb_spec.rb
@@ -16,11 +16,18 @@ RSpec.describe 'User Show', type: :feature do
 
     visit user_path(@user.id)
   end
-  
+
   it 'shows the right content' do
     expect(page).to have_content(@user.name)
     expect(page).to have_css("img[src*='#{@user.photo}']")
     expect(page).to have_content("Number of posts: #{@user.posts_counter}")
     expect(page).to have_content(@user.bio)
+  end
+
+  it 'shows the last three posts' do
+    @posts.each do |post|
+      expect(page).to have_content(post.title)
+    end
+    expect(page).to_not have_content(@post1.title)
   end
 end
